c7714fffc23b5ca442f3359bfb02bf8737493103
Pierre Kestener <pierre.kestener@cea.fr>
2017-04-08 15:11:10 +0200

    remove use of deprecated MPI Cpp API

--- test/mpiBasic/testMpiHelloGlobalSession.cpp
+++ test/mpiBasic/testMpiHelloGlobalSession.cpp
@@ -17,38 +17,28 @@
 #include <ctime>
 
 #include <GlobalMpiSession.h>
+#include <MpiComm.h>
 
 int main(int argc,char **argv)
 {
   int myRank, numProcs, namelength;
   char processor_name[MPI_MAX_PROCESSOR_NAME+1];
 
-  time_t         curtime;
-  struct tm     *loctime;
-
   // MPI resources
   hydroSimu::GlobalMpiSession mpiSession(&argc,&argv);
 
-  numProcs  = MPI::COMM_WORLD.Get_size();
-  myRank    = MPI::COMM_WORLD.Get_rank();
-  try {
-    MPI::Get_processor_name(processor_name,namelength);
-  } catch (MPI::Exception e) {
-    std::cout << "MPI ERROR: " << e.Get_error_code()	\
-  	      << " -" << e.Get_error_string()		\
-  	      << std::endl;
-    std::cout << "Unable to get processor name; set to \" unknown\" \n";
-    strcpy(processor_name, "unknown");
-  }
+  numProcs  = hydroSimu::MpiComm::world().getNProc();
+  myRank    = hydroSimu::MpiComm::world().getRank();
+
+  MPI_Get_processor_name(processor_name, &namelength);
   
   // print local time on rank 0 machine
   if ( myRank == 0 ) {
-    curtime = time (NULL);
-    loctime = localtime (&curtime);
-    std::cout << "Local time of process 0 : " << asctime (loctime) << std::endl;
+    std::time_t curtime = std::time (NULL);
+    std::cout << "Local time of process 0 : " << std::asctime (std::localtime (&curtime)) << std::endl;
   }
   
-  // // print process rank and hostname
+  // print process rank and hostname
   std::cout << "MPI process " << myRank << " of " << numProcs << " is on " <<
     processor_name << std::endl;
 
